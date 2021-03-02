4b8aff50b2b6f93f66ac77e9fe883687a4a5279f
SHIV SAGAR SHAH <71564583+ShivSagarShah@users.noreply.github.com>
2021-03-02 15:46:40 +0530

    Update selection.cpp

--- selection.cpp
+++ selection.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 int main()
 {
-    int arr[100], min;
+    int min;
     cout << "enter the number of elements:" << endl;
 
     int n;
 
     cin >> n;
+    int arr[n];
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
